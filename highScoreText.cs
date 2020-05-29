a3f6dd533917e42cb222c628031bd952e0246649
Zansood <60878775+Zansood@users.noreply.github.com>
2020-05-29 07:09:34 +0700

    Add files via upload

---
+++ highScoreText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class highScoreText : MonoBehaviour
+{
+    Text highScore;
+    void Start()
+    {
+        highScore = GetComponent<Text>();
+        highScore.text = "High Score: " +PlayerPrefs.GetInt("HighScore").ToString();
+    }
+}
