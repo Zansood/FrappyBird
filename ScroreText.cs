2586e323f697cf0a7f9fee90a96fd5c4c59cc0ed
Zansood <60878775+Zansood@users.noreply.github.com>
2020-05-29 07:10:16 +0700

    Add files via upload

---
+++ ScroreText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScroreText : MonoBehaviour
+{
+    Text score;
+    void Start()
+    {
+        score = GetComponent<Text>();
+        score.text = "Score: " +GameManager.Instance.Score;
+    }
+}
